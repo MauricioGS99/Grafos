efb60b4ac3a4606fe99a637a648ccbecaa6e37ce
MauricioGS99 <m.gs9989@gmail.com>
2016-01-18 18:50:59 -0600

    Script de ejemplo para Graph Tool

---
+++ Python_Script.py
@@ -0,0 +1,11 @@
+#!
+
+from __future__ import division, absolute_import
+import sys
+range = xrange
+import os
+from pylab import *
+from numpy.random import *
+
+#Graph tool
+from graph_tool.all import *
